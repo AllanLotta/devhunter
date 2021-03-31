b99b8c961cd0fb63d2c4e5417bfafc4f25859538
Allan Lotta <allan.lopes@multivarejogpa.com.br>
2021-03-31 16:32:21 -0300

    added home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Container, Content, SeeJobsButton } from './styles';
 
-import { Container } from './styles';
-
-const PostJob: React.FC = () => {
-  return <Container>PostJob Page</Container>;
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <h1>
+          Search online <span>millions of jobs</span> <br /> to find the next
+          step in your career.
+        </h1>
+        <SeeJobsButton>See Jobs</SeeJobsButton>
+      </Content>
+      <img
+        src="https://d4zjpv0aa4kr2.cloudfront.net/assets/home/man-yellow-shirt-b77f31c40fff206cf3d8ed7feb6855cd7cf8d84fca30313a6cf2e679f4657796.png"
+        alt="Girl coding"
+      />
+    </Container>
+  );
 };
 
-export default PostJob;
+export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,71 @@
 import styled from 'styled-components';
+import { Colors } from '../../styles/colors';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 1120px;
+  height: calc(100vh - 5rem); //5rem altura do cabecalho
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin: 0 auto;
+  padding: 0 2rem;
+
+  @media (max-width: 768px) {
+    height: unset;
+    img {
+      display: none;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  max-width: 600px;
+
+  h1 {
+    font-size: 4.5rem;
+    font-weight: 900;
+
+    line-height: 4.5rem;
+    margin-top: 2.5rem;
+
+    span {
+      color: ${Colors.cyan500};
+    }
+  }
+
+  > button {
+    margin-top: 2.5rem;
+  }
+
+  @media (max-width: 768px) {
+    h1 {
+      font-size: 3rem;
+    }
+  }
+`;
+
+export const SeeJobsButton = styled.button`
+  width: 260px;
+  height: 4rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 0;
+  border-radius: 2rem;
+
+  background: ${Colors.yellow500};
+  color: ${Colors.gray900};
+
+  font-size: 1.25rem;
+  font-weight: bold;
+
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
+`;
