b8d6874f19c4d8a0e2ea34f18dbd815fe5cd228b
Allan Lotta <allan.lopes@multivarejogpa.com.br>
2021-03-31 18:53:55 -0300

    added Jobs pages

--- src/pages/Jobs/Job/index.tsx
+++ src/pages/Jobs/Job/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 const Job: React.FC = () => {
-  return <Container>Job Page</Container>;
+  return (
+    <Container>
+      <Content>
+        <div>
+          <span>FrontEnd</span>
+          <strong>Front End Developer (REMOTE)</strong>
+          <p>
+            Building client facing mobile applications, focusing on high
+            performance, beautiful UI and code quality. Collaborate with other
+            tech, product, and design people to identify and solve challenging
+            problems, creating awesome experiences for users worldwide
+          </p>
+          <span>R$10000 - R$14000</span>
+        </div>
+      </Content>
+    </Container>
+  );
 };
 
 export default Job;
--- src/pages/Jobs/styles.ts
+++ src/pages/Jobs/styles.ts
@@ -1,3 +1,61 @@
 import styled from 'styled-components';
+import { Colors } from '../../styles/colors';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 2rem;
+`;
+
+export const Content = styled.div`
+  max-width: 900px;
+  padding-bottom: 50px;
+  margin: 5rem auto 0;
+
+  span {
+    display: block;
+    font-size: 1rem;
+    color: ${Colors.gray300};
+    margin-top: 1.5rem;
+  }
+
+  div {
+    margin-top: 2rem;
+    line-height: 2rem;
+    padding: 2rem;
+    font-size: 1.125rem;
+    color: ${Colors.gray100};
+    border: 1px solid ${Colors.gray700};
+    border-radius: 5px;
+
+    p {
+      display: -webkit-box;
+      -webkit-line-clamp: 1;
+      -webkit-box-orient: vertical; /* space for ellipsis */
+      overflow: hidden;
+    }
+    button {
+      margin-top: 20px;
+      padding: 5px 20px;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      border: 0;
+      border-radius: 2rem;
+
+      background: ${Colors.yellow500};
+      color: ${Colors.gray900};
+
+      font-size: 1.1rem;
+      font-weight: bold;
+
+      transition: filter 0.2s;
+
+      &:hover {
+        filter: brightness(0.8);
+      }
+    }
+  }
+`;
