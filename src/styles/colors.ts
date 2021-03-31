fd0cc41c760e715a0db9e2b00dda10508e7db9f8
Allan Lotta <allan.lopes@multivarejogpa.com.br>
2021-03-31 16:35:34 -0300

    added default colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  white: '#ffffff',
+  gray100: '#e1e1e6',
+  gray300: '#a8a8b3',
+  gray700: '#323238',
+  gray800: '#29292e',
+  gray850: '#1f2729',
+  gray900: '#121214',
+
+  cyan500: '#61dafb',
+  yellow500: '#eba417',
+};
