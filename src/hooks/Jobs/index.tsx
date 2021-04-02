d6997b6b8e42ede343a7213b2296c2f73bfad9ce
Allan Lotta <allan.lopes@multivarejogpa.com.br>
2021-04-02 19:43:27 -0300

    added jobs hooks

--- src/hooks/Jobs/index.tsx
+++ src/hooks/Jobs/index.tsx
@@ -1,17 +1,90 @@
-import React, { createContext, useState, useContext } from 'react';
+import axios from 'axios';
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useCallback,
+  useEffect,
+} from 'react';
+import { IJob } from '../../pages/Jobs/interfaces';
+import { baseURL } from '../../services/api';
+import IJobsContextData from './interface';
 
-import IJobsContext from './interface';
-
-const JobsContext = createContext<IJobsContext>({} as IJobsContext);
+const JobsContext = createContext<IJobsContextData>({} as IJobsContextData);
 
 const JobsProvider: React.FC = ({ children }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState<IJob[]>([] as IJob[]);
+
+  useEffect(() => {
+    const loadJobs = async (): Promise<void> => {
+      const response = await axios.get(`${baseURL}/jobs`);
+      setJobs(response.data);
+    };
+    loadJobs();
+  }, []);
+
+  const getJobById = useCallback(async (id: number): Promise<IJob | null> => {
+    try {
+      const response = await axios.get(`${baseURL}/jobs/${id}`);
+
+      return response.data;
+    } catch (error) {
+      return null;
+    }
+  }, []);
+
+  const postJob = useCallback(async (data: IJob): Promise<boolean> => {
+    try {
+      const response = await axios.post(`${baseURL}/jobs`, data, {
+        headers: {
+          'Content-type': 'application/json; charset=utf-8',
+        },
+      });
+      if (response.data.id) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      return false;
+    }
+  }, []);
+
+  const editJob = useCallback(async (data: IJob): Promise<boolean> => {
+    try {
+      const response = await axios.put(`${baseURL}/jobs/${data.id}`, data, {
+        headers: {
+          'Content-type': 'application/json; charset=utf-8',
+        },
+      });
+      if (response.data.id) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      return false;
+    }
+  }, []);
+
+  const deleteJob = useCallback(async (id: number): Promise<boolean> => {
+    try {
+      const response = await axios.delete(`${baseURL}/jobs/${id}`);
+      if (response.data.id) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      return false;
+    }
+  }, []);
 
   return (
     <JobsContext.Provider
       value={{
         jobs,
+        getJobById,
+        postJob,
+        editJob,
+        deleteJob,
       }}
     >
       {children}
@@ -19,8 +92,14 @@ const JobsProvider: React.FC = ({ children }) => {
   );
 };
 
-const JobsCreateContext = (): IJobsContext => {
-  return useContext(JobsContext);
-};
+function useJobs(): IJobsContextData {
+  const context = useContext(JobsContext);
 
-export { JobsContext, JobsProvider, JobsCreateContext };
+  if (!context) {
+    throw new Error('useJobs must be used within an JobsProvider');
+  }
+
+  return context;
+}
+
+export { JobsProvider, useJobs };
--- src/hooks/Jobs/interface.ts
+++ src/hooks/Jobs/interface.ts
@@ -1,3 +1,9 @@
-export default interface IJobsContext {
-  jobs: unknown[];
+import { IJob } from '../../pages/Jobs/interfaces';
+
+export default interface IJobsContextData {
+  jobs: IJob[];
+  getJobById: (id: number) => Promise<IJob | null>;
+  postJob: (data: IJob) => Promise<boolean>;
+  editJob: (data: IJob) => Promise<boolean>;
+  deleteJob: (id: number) => Promise<boolean>;
 }
