aa524626513b8b42fbd7a837979dc83a8fb0b812
Allan Lotta <allan.lopes@multivarejogpa.com.br>
2021-04-02 23:34:42 -0300

    fixed colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,11 +2,13 @@ export const Colors = {
   white: '#ffffff',
   gray100: '#e1e1e6',
   gray300: '#a8a8b3',
+  gray600: '#666360',
   gray700: '#323238',
   gray800: '#29292e',
   gray850: '#1f2729',
   gray900: '#121214',
 
   cyan500: '#61dafb',
-  yellow500: '#eba417',
+  yellow500: '#ff9000',
+  red600: '#c53030',
 };
